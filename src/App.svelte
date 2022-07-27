ea0b7b8eb4b3b1b02c81137f9769733708f3b5b5
Moos Toet <moostoet@hotmail.com>
2022-07-27 20:03:51 +0200

    :)

--- src/App.svelte
+++ src/App.svelte
@@ -8,6 +8,7 @@
     <div class="bg-container-dark p-5 w-1/2 rounded-lg shadow-md">
       <header class="w-full flex flex-row items-center">
         <p class="font-bold text-xl flex-1">Hey there. 👋</p>
+        <p>Martin = stinky nerd. 🤓</p>
         <Icon path={mdiWindowClose} color="white"/>
       </header>
     </div>
